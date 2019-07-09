8ee146dfd6a59c4cd164a96bf07a29a1f0139f10
Mehul Raj Kumawat <mehul.kumawat1804@gmail.com>
2019-07-09 10:10:55 +0530

    Added Filter Feature on tags and ingredients to recipi

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -18,7 +18,18 @@ class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
 
     def get_queryset(self):
         """Return objects for the current authenticated user"""
-        return self.queryset.filter(user=self.request.user).order_by('-name')
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+            # string no has to be converted to int hence, it will be true only
+            # default value passed as the expr may be NoneType
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+        # returning duplicate so a distinct keyword needs to be used.
+        return queryset.filter(
+            user=self.request.user
+            ).order_by('-name').distinct()
 
     def perform_create(self, serializer):
         """Create new object"""
